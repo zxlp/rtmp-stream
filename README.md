985e48f98bcb2e0a63de91190fd21f55010aa84b
zxlp <100291976+zxlp@users.noreply.github.com>
2022-03-02 20:08:24 +0530

    add: custom `PRESET` and `CRF`

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cuddly-garbanzo
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
--- stream.sh
+++ stream.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 INPUT_URL="${INPUT_URL:-https://magselect-stirr.amagi.tv/playlist1080p.m3u8}"
+PRESET="${PRESET:-veryfast}"
+CRF="${CRF:-24}"
 
 if [[ -z "${SERVER_URL}" ]]; then
     echo "SERVER_URL is not set"
@@ -14,4 +16,4 @@ elif [[ $INPUT_URL =~ ^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$ ]];
 fi
 
 printf "PREPARING TO STREAM \n"
-ffmpeg -i $INPUT_URL -preset veryfast -tune zerolatency -f flv -c:v libx264 -c:a aac -r 30 -crf 24 -vf "scale=-1:1080" $SERVER_URL$STREAM_KEY
+ffmpeg -i $INPUT_URL -preset $PRESET -tune zerolatency -f flv -c:v libx264 -c:a aac -r 30 -crf $CRF -vf "scale=-1:1080" $SERVER_URL$STREAM_KEY
